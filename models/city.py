2e6ba77fd97ff3699510c9bfb04a474df471cf4f
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-08-08 16:08:30 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -5,5 +5,11 @@
 from models.base_model import BaseModel
 
 class City(BaseModel):
+        """Represent a city.
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
+
     state_id = ""
     name = ""
