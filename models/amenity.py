876bce616e6065fefeabe444e718913761ab79f2
root <kevohm>
2022-08-08 13:02:37 +0000

    done

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+    class Ameity
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+    State class
+"""
+from models.base_model import BaseModel
+class State:
+    """ inside State class """
+    name = ""
